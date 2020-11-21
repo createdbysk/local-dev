6540e1b6943e9368b733895bf3b25dc7c26e0c41
Satish Kumar <kumarnotifications@gmail.com>
2020-11-21 14:19:20 -0600

    Add a repo1 flask app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # local-dev
+
+Implements a consistent local-dev experience across any programming language.
---
+++ experiments/multiple-repos/repo1/src/app.py
@@ -0,0 +1,19 @@
+import flask
+import os
+
+app = flask.Flask(__name__)
+
+@app.route(
+    '/',
+    methods=["POST"],
+    strict_slashes=False,
+)
+def main():
+    what = os.environ.get("WHAT", "repo1")
+    return f"Hello {what}"
+
+
+if __name__ == "__main__":
+    # The host=0.0.0.0 is required for this to work correctly
+    # within a docker container with ports forwarded.
+    app.run(host="0.0.0.0")
